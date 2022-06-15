3a16737f99e7c18b4c8d1a9a40e18f222ad2206b
Sof1ane <43276871+Sof1ane@users.noreply.github.com>
2022-06-15 11:07:37 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,13 +15,18 @@ villeneuve_papier = ['papier','cardboard']
 
 villeneuve_plastique = ['plastic','glass','metal']
 
-# villeneuve_organique = ['organic']
+villeneuve_organique = ['organic']
 
 
 def load_model():
     model = keras.models.load_model('modele_nounou')
     return model
 
+option = st.selectbox(
+     'Dans quelle ville résidez vous ?',
+     ('Lille', "Villeneuve d'Ascq"))
+
+st.write('You selected:', option)
 
 def load_image():
     uploaded_file = st.file_uploader(label='Pick an image to test')
@@ -33,17 +38,31 @@ def load_image():
         return None
 
 
-def predict(model, image):
+def predict(model, image,option):
     img = keras.preprocessing.image.load_img(image, target_size=(256, 256))
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
     pdc = model.predict(img_array)
     # st.write(pdc[0]*100 , "\n", classes)
-    if classes[np.argmax(pdc)] in recyclable_lille:
-        st.write("recyclable", pdc[0][np.argmax(pdc)])
+    if option == 'Lille':
+        if classes[np.argmax(pdc)] in recyclable_lille:
+            st.write("recyclable", classes[np.argmax(pdc)]])
 
-    else:
-        st.write("non recyclable", pdc[0][np.argmax(pdc)])
+        else:
+            st.write("non recyclable", classes[np.argmax(pdc)]])
+
+    if option =="Villeneuve d'Ascq":
+        if classes[np.argmax(pdc)] in villeneuve_papier:
+            st.write("Compartiment papier", classes[np.argmax(pdc)])
+
+        elif classes[np.argmax(pdc)] in villeneuve_plastique:
+            st.write("Compartiment plastique / conserve / verre", classes[np.argmax(pdc)])
+
+        elif classes[np.argmax(pdc)] in villeneuve_organique:
+            st.write("Compartiment déchets organiques", classes[np.argmax(pdc)])
+
+        else:
+            st.write("Compartiment non recyclable", classes[np.argmax(pdc)])
 
     # st.write("Prediction: ", classes[np.argmax(pdc)], pdc[0][np.argmax(pdc)])
         
